f9bc8b20049f394482f970f3243ef6c11e311e32
Olamide <oladimejiolamide76@gmail.com>
2025-12-05 04:21:49 +0100

    feat: JS script to print the required three lines sequentially

---
+++ 1-multi_languages.js
@@ -0,0 +1,9 @@
+/*
+ * three_lines.js
+ * Script that prints three specific lines.
+ * Constraints: Must use console.log, no 'var'.
+ */
+
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
