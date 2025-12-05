a4eb469a5f91b32fc36c5265e141f1aa7a5235e4
Olamide <oladimejiolamide76@gmail.com>
2025-12-05 03:15:51 +0100

    Add 2-arguments.js to handle argument count

---
+++ 2-arguments.js
@@ -0,0 +1,12 @@
+// 2-arguments.js
+
+const args = process.argv.slice(2); // Get all arguments after 'node' and script name
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
+
